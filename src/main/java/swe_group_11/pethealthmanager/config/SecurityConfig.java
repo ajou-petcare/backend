0748976767fa6d89d87994f00affa2ad25a4f078
hyeonseob22 <97226053+LeeHyeonseob@users.noreply.github.com>
2023-11-28 16:05:24 +0900

    fix error2

---
+++ src/main/java/swe_group_11/pethealthmanager/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package swe_group_11.pethealthmanager.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
