585165ca947067c6ca8b7b49aa43dc0117b1fcbd
hyeonseob22 <97226053+LeeHyeonseob@users.noreply.github.com>
2023-12-01 12:58:02 +0900

    fix error 4

--- src/main/java/swe_group_11/pethealthmanager/DTO/PetDTO.java
+++ src/main/java/swe_group_11/pethealthmanager/DTO/PetDTO.java
@@ -1,11 +1,8 @@
 package swe_group_11.pethealthmanager.DTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 @Data
-@RequiredArgsConstructor
 public class PetDTO {
     private Long id;
     private Long ownerId;
